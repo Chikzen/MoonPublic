31dea9db21a289ec398e610aff83b287a827933b
Chikzen <41597070+Chikzen@users.noreply.github.com>
2023-03-15 00:39:42 +0200

    Update installMoonBot.ps1

--- installMoonBot.ps1
+++ installMoonBot.ps1
@@ -1,13 +1,13 @@
 # Функция установки MoonBot
 function func_install_MoonBot 
 {   Write-Host Install MoonBot -ForegroundColor Green -BackgroundColor Black
-    if (-Not (Test-Path -Path "C:\Soft\Moon\MoonBot.exe" -PathType Leaf)) {
+    if (-Not (Test-Path -Path "C:\Moon\MoonBot.exe" -PathType Leaf)) {
         if (-Not (Test-Path MoonBot.zip -PathType Leaf)) {
             Write-Host Download MoonBot -ForegroundColor Cyan
             Invoke-WebRequest -O MoonBot.zip "https://moon-bot.com/files/MoonBot.zip"
         }
         if (Test-Path MoonBot.zip -PathType Leaf) {
-            Expand-Archive -Path MoonBot.zip "C:\Soft\Moon"
+            Expand-Archive -Path MoonBot.zip "C:\Moon"
             Remove-Item .\MoonBot.zip
         }
     }
