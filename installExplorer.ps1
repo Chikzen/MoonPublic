043e343725ec4984abe34205f642aea0e12afb5e
Chikzen <41597070+Chikzen@users.noreply.github.com>
2023-03-16 16:36:11 +0200

    Update installExplorer.ps1

--- installExplorer.ps1
+++ installExplorer.ps1
@@ -1,17 +1,17 @@
 #Установка и запуск Explorer++
-New-Item -Path 'C:\Explorer\' -ItemType Directory
+New-Item -Path 'C:\Soft\Explorer\' -ItemType Directory
 function func_install_Explorer {
     Write-Host Install Explorer -ForegroundColor Green -BackgroundColor Black
-    if (-Not (Test-Path -Path "C:\Explorer\Explorer++.exe" -PathType Leaf)) {
+    if (-Not (Test-Path -Path "C:\Soft\Explorer\Explorer++.exe" -PathType Leaf)) {
         if (-Not (Test-Path explorerpp_x64.zip -PathType Leaf)) {
             Write-Host Download Explorer -ForegroundColor Cyan
             Invoke-WebRequest -O explorerpp_x64.zip "https://github.com/derceg/explorerplusplus/releases/download/version-1.4.0-beta-2/explorerpp_x64.zip"
         }
         if (Test-Path explorerpp_x64.zip -PathType Leaf) {
-            Expand-Archive -Path explorerpp_x64.zip "C:\Explorer"
+            Expand-Archive -Path explorerpp_x64.zip "C:\Soft\Explorer"
             Remove-Item .\explorerpp_x64.zip
         }
     }
 }
 func_install_Explorer
-start c:\Explorer\explorer++.exe
+start c:\Soft\Explorer\explorer++.exe
