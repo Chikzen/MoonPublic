91cf938fca01def1fa46e0c605fc00f64e6e7030
Chikzen <41597070+Chikzen@users.noreply.github.com>
2023-03-16 16:31:23 +0200

    Add files via upload

---
+++ installMemReduct.ps1
@@ -0,0 +1,26 @@
+# Уcтановка и запуск MemReduct
+#Write-Host Download MemReduct -ForegroundColor Green -BackgroundColor Black
+#Invoke-WebRequest -outfile 'c:\soft\mem.zip' #"https://github.com/Chikzen/MoonPublic/raw/271589be4712d81503072d258cd399466df2a310/Mem.zip"
+#Expand-Archive c:\soft\mem.zip
+#Remove-Item c:\soft\mem.zip
+#Start-Process -FilePath "memreduct.exe" -WorkingDirectory 'C:\Soft\MemReduct'
+
+
+
+# Функция установки MemReduct
+function func_install_MemReduct
+{   Write-Host Install MemReduct -ForegroundColor Green -BackgroundColor Black
+    if (-Not (Test-Path -Path "C:\Soft\MemReduct\memreduct.exe" -PathType Leaf)) {
+        if (-Not (Test-Path mem.zip -PathType Leaf)) {
+            Write-Host Download MemReduct -ForegroundColor Cyan
+            Invoke-WebRequest -O mem.zip "https://github.com/Chikzen/MoonPublic/raw/271589be4712d81503072d258cd399466df2a310/Mem.zip"
+        }
+        if (Test-Path mem.zip -PathType Leaf) {
+            Expand-Archive -Path mem.zip "C:\Soft"
+            Remove-Item .\mem.zip
+        }
+    }
+} 
+    
+# Установка MoonBot
+func_install_MemReduct
