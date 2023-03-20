ffec61277ab43551642439e5491510f6cddab286
Chikzen <41597070+Chikzen@users.noreply.github.com>
2023-03-20 02:05:34 +0200

    Update ROGER_InstallSoftware.ps1

--- ROGER_InstallSoftware.ps1
+++ ROGER_InstallSoftware.ps1
@@ -69,6 +69,45 @@ choco install notepad3 -y -r
 #$ScriptPath = ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/DigitalRuby/IPBan/master/IPBanCore/Windows/Scripts/install_latest.ps1'))
 #Invoke-Command -ScriptBlock ([scriptblock]::Create($ScriptPath)) -Args "silent", $true
 
+
+#Установка и запуск Explorer++
+New-Item -Path 'C:\Soft\Explorer\' -ItemType Directory
+function func_install_Explorer {
+    Write-Host Install Explorer -ForegroundColor Green -BackgroundColor Black
+    if (-Not (Test-Path -Path "C:\Soft\Explorer\Explorer++.exe" -PathType Leaf)) {
+        if (-Not (Test-Path explorerpp_x64.zip -PathType Leaf)) {
+            Write-Host Download Explorer -ForegroundColor Cyan
+            Invoke-WebRequest -O explorerpp_x64.zip "https://github.com/derceg/explorerplusplus/releases/download/version-1.4.0-beta-2/explorerpp_x64.zip"
+        }
+        if (Test-Path explorerpp_x64.zip -PathType Leaf) {
+            Expand-Archive -Path explorerpp_x64.zip "C:\Soft\Explorer"
+            Remove-Item .\explorerpp_x64.zip
+        }
+    }
+}
+func_install_Explorer
+#start c:\Soft\Explorer\explorer++.exe
+
+# Изменение часового пояса
+$tmZone = "FLE Standard Time"
+$WinOSVerReg = Get-Item "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion"
+$WinOSVer = $WinOSVerReg.GetValue("CurrentVersion")
+if ($WinOSVer -GE 6){
+tzutil.exe /s $tmZone
+} Else {
+$param = "/c Start `"Change tmZone`" /MIN %WINDIR%\System32\Control.exe TIMEDATE.CPL,,/Z "
+$param += $tmZone
+$proc = [System.Diagnostics.Process]::Start( "CMD.exe", $param )
+}
+
+# Открытие и смена RDP 
+cscript C:\Windows\System32\Scregedit.wsf /ar 0
+Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp\" -Name PortNumber -Value 31542
+New-NetFirewallRule -DisplayName "RDP-31542" -Direction Inbound -LocalPort 31542 -Protocol TCP -Action allow
+New-NetFirewallRule -DisplayName "RDP-31542" -Direction Inbound -LocalPort 31542 -Protocol UDP -Action allow
+#net stop termservice
+#net start termservice
+
 switch (Read-Host 'Restart computer now? [y/n]') {
     y { Restart-computer -Force -Confirm:$false }
     n { Write-Host 'Restart cancelled...' -ForegroundColor Yellow -NoNewline }
