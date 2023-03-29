1442e592c75eed2080d80f513c958c6f03738e0c
Chikzen <41597070+Chikzen@users.noreply.github.com>
2023-03-29 16:27:39 +0300

    Update installOpenSSH.ps1

--- installOpenSSH.ps1
+++ installOpenSSH.ps1
@@ -1,5 +1,4 @@
 # Уcтановка, настройка и запуск OpenSSH
-Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 choco install openssh -params '"/SSHServerFeature"'
 refreshenv
 #Enable-PSRemoting
