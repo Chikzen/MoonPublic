45d85c016fc6061d7534f245140aa30dfd5e95fe
Chikzen <41597070+Chikzen@users.noreply.github.com>
2024-08-15 10:13:25 +0300

    Update cleaning.ps1

--- cleaning.ps1
+++ cleaning.ps1
@@ -10,10 +10,14 @@ Get-ChildItem -Path "C:\Windows\Logs\*" -Recurse | ForEach-Object { Remove-Item 
 # Очищення кешу Windows Update
 Stop-Service -Name wuauserv -Force
 Get-ChildItem -Path "C:\Windows\SoftwareDistribution\Download\*" -Recurse | ForEach-Object { Remove-Item $_.FullName -Recurse -Force -ErrorAction SilentlyContinue }
-Start-Service -Name wuauserv
+#Start-Service -Name wuauserv
 
 # Видалення старих точок відновлення (запуск від імені адміністратора)
 vssadmin delete shadows /for=C: /all /quiet
 
 # Видалення не потрібних пакетів
 dism.exe /online /cleanup-image /startcomponentcleanup /resetbase
+
+cd C:\Soft\NodeJsProg
+npm init -y
+npm install express basic-auth sqlite3 qckwinsvc fs-extra express-basic-auth axios 
